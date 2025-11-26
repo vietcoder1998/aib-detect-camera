010bb86e7983f0fd6370adbf7b0588c45662df81
vietcoder1998 <duyviet2841998@gmail.com>
2025-11-26 15:43:53 +0700

    feature: full of image

--- src/components/atoms/EquipmentCard.tsx
+++ src/components/atoms/EquipmentCard.tsx
@@ -9,22 +9,22 @@ interface EquipmentCardProps {
 
 export const EquipmentCard: React.FC<EquipmentCardProps> = ({ title, specs, imageUrl }) => {
   return (
-    <Card className="overflow-hidden">
-      <div className="flex flex-col md:flex-row">
+    <Card className="!p-0 overflow-hidden">
+      <div className="flex flex-col md:flex-row h-full">
         {imageUrl && (
-          <div className="w-full md:w-1/3 h-48 md:h-full md:min-h-[300px] bg-gray-100 overflow-hidden flex-shrink-0">
+          <div className="w-full md:w-1/3 h-48 md:h-auto md:min-h-[300px] bg-gray-100 flex-shrink-0 relative overflow-hidden">
             <img 
               src={imageUrl} 
               alt={title}
-              className="w-full h-full object-cover hover:scale-105 transition-transform duration-300"
+              className="absolute inset-0 w-full h-full object-cover hover:scale-105 transition-transform duration-300"
             />
           </div>
         )}
-        <div className="p-6 flex-1">
+        <div className="p-6 flex-1 flex flex-col">
           <CardHeader>
             {title}
           </CardHeader>
-          <SpecList>
+          <SpecList className="flex-1">
             {Object.entries(specs).map(([key, value]) => (
               <SpecItem key={key} label={key} value={value} />
             ))}
